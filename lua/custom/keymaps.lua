8800fe3cb3f629a84bbfdac34a11346b1b7a4d22
Brent Pearce <brentmp2@gmail.com>
2024-07-15 21:59:27 -0500

    Added plugins and keymaps to work with splits better.
    Zoom in and out of splits with <Leader>mm for maximize. :)
    Choose active tabs and splits with <Leader>cw for choose window. :)

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,1 +1,5 @@
-vim.keymap.set('n', '<Leader>gs', '<cmd>Git<CR>', { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', '<Leader>gs', '<cmd>Git<CR>', { desc = '[G]it [S]tatus' })
+
+vim.keymap.set('n', '<Leader>mm', '<cmd>MaximizerToggle!<CR>', { desc = '[M]axi[m]ize Toggle' })
+
+vim.keymap.set('n', '<Leader>cw', '<Plug>(choosewin)', { desc = 'Choose Window or Tab' })
---
+++ lua/custom/plugins/splits.lua
@@ -0,0 +1,4 @@
+return {
+  { 'szw/vim-maximizer' },
+  { 't9md/vim-choosewin' },
+}
